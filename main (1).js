2dceb0831e0a01903795da372d1ddecfc4b3e82f
imawsome1234567 <90875942+imawsome1234567@users.noreply.github.com>
2022-03-08 17:52:31 -0800

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,18 @@
+var SpeechRecognition = window.wedkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+console.log(event);
+
+var Content = event.results[0][0].transcript;
+
+document.getElementById("textbox").innerHTML=Content;
+}
